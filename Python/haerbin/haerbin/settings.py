51b45b09f3105f40c225aa61f08b31608690f3ab
root <root@localhost.localdomain>
2014-10-17 23:08:25 +0800

    crawk haerbin

---
+++ Python/haerbin/haerbin/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for haerbin project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'haerbin'
+
+SPIDER_MODULES = ['haerbin.spiders']
+NEWSPIDER_MODULE = 'haerbin.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'haerbin (+http://www.yourdomain.com)'
---
+++ Python/haerbin/haerbin/spiders/hrbcb.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import bs4
+from bs4 import BeautifulSoup
+
+
+class HrbcbSpider(scrapy.Spider):
+    name = "hrbcb"
+    allowed_domains = ["www.hrbcb.com.cn"]
+    start_urls = (
+        'http://www.hrbcb.com.cn/card/card5.jsp',
+    )
+
+    def parse(self, response):
+        htmlResponse = ''.join(response.body)
+        soup = BeautifulSoup(htmlResponse)
+        ''' silly method, but worked
+        cardDiv = soup.find(id="card").find_all("p")
+        for li in cardDiv:
+            #print "type is " + str(type(li))
+            tmpStr =  str(li)
+            print tmpStr.replace("span","").replace("p", "").replace("br","").replace("<","").replace(">","").replace("/","").replace("class","").replace('"',"").replace("hometitle","").replace("=","").replace(" " , "")'''
+
+
+        for item in soup.find_all('p'):
+            for des in item.descendants:
+                if isinstance(des, bs4.element.NavigableString):
+                    print des.strip()
